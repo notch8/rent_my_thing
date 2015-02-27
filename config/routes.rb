13c29678285be4d0401660dde2aa2de418fac7bd
empireofryan <ryan@ryanclifton.com>
2015-02-27 13:49:57 -0800

    Login and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :categories do
-    resources :posting
+    resources :postings
   end
 
+  resources :postings
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
