a601cd7e41157723eacbd6d70ebad42ec17a5908
Kyle Gray <jetamartin@yahoo.com>
2015-03-02 14:15:17 -0800

    DB schema changes, Posting change for addresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227192939) do
+ActiveRecord::Schema.define(version: 20150302200844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,15 +26,16 @@ ActiveRecord::Schema.define(version: 20150227192939) do
     t.string    "title"
     t.text      "description"
     t.integer   "category_id"
-    t.decimal   "rate",                      precision: 8, scale: 2
+    t.decimal   "rate",                       precision: 8, scale: 2
     t.daterange "available_dates"
-    t.string    "street",          limit: 2
+    t.string    "street",          limit: 20
     t.string    "zip",             limit: 5
     t.string    "phone"
     t.string    "email"
-    t.datetime  "created_at",                                        null: false
-    t.datetime  "updated_at",                                        null: false
-    t.string    "state"
+    t.datetime  "created_at",                                         null: false
+    t.datetime  "updated_at",                                         null: false
+    t.string    "state",           limit: 2
+    t.string    "city",            limit: 30
   end
 
   add_index "postings", ["category_id"], name: "index_postings_on_category_id", using: :btree
