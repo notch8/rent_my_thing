e407f407989a34a9492232c0bae230f7adcb14cc
LEARN01 <learn01@LEARN01.local>
2015-03-02 09:29:27 -0800

    got date range input working

--- app/assets/javascripts/postings.js
+++ app/assets/javascripts/postings.js
@@ -1,11 +1,6 @@
 $(document).on ('page:change', function() {
 
-  $('#reservation').daterangepicker({
-     timePicker: true,
-     timePickerIncrement: 30,
-     format: 'MM/DD/YYYY h:mm A'
-   }, function(start, end, label) {
-     console.log(start.toISOString(), end.toISOString(), label);
-   });
+  $( "#from-date" ).datepicker();
+  $( "#to-date" ).datepicker();
 
 })
