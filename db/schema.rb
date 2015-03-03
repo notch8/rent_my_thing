bcaac6e38ccb9afad2834947f0bf646c133f6f13
kylegray2 <kylegray2@outlook.com>
2015-03-03 13:47:32 -0800

    support image uploading to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150302223331) do
+ActiveRecord::Schema.define(version: 20150303185728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,16 +26,20 @@ ActiveRecord::Schema.define(version: 20150302223331) do
     t.string    "title"
     t.text      "description"
     t.integer   "category_id"
-    t.decimal   "rate",                       precision: 8, scale: 2
+    t.decimal   "rate",                          precision: 8, scale: 2
     t.daterange "available_dates"
-    t.string    "street",          limit: 20
-    t.string    "zip",             limit: 5
+    t.string    "street",             limit: 20
+    t.string    "zip",                limit: 5
     t.string    "phone"
     t.string    "email"
-    t.datetime  "created_at",                                         null: false
-    t.datetime  "updated_at",                                         null: false
-    t.string    "state",           limit: 2
-    t.string    "city",            limit: 30
+    t.datetime  "created_at",                                            null: false
+    t.datetime  "updated_at",                                            null: false
+    t.string    "state",              limit: 2
+    t.string    "city",               limit: 30
+    t.string    "image_file_name"
+    t.string    "image_content_type"
+    t.integer   "image_file_size"
+    t.datetime  "image_updated_at"
   end
 
   add_index "postings", ["category_id"], name: "index_postings_on_category_id", using: :btree
