d3f00f7c49cdc3d1d29fdc30c0bf8d95cdfe4851
empireofryan <ryan@ryanclifton.com>
2015-03-02 16:53:58 -0800

    Add an egregious hack that gets the date range picker working

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,4 +1,5 @@
 class PostingsController < ApplicationController
+  before_action :set_date_range, only: [:create, :update]
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy ]
   before_action :load_categories, only: [:show, :edit, :new ]
   before_action :set_posting, only: [:show, :edit, :update, :destroy]
@@ -26,9 +27,12 @@ class PostingsController < ApplicationController
   # POST /postings
   # POST /postings.json
   def create
+    fields = posting_params
+    fields[:available_dates] = @available_dates if @available_dates
+
     params["posting"]["category_id"] = params["Category"]
     # logger.debug "date range: #{params["rentrange"]["from"]} to #{params["rentrange"]["to"]} "
-    @posting = Posting.new(posting_params)
+    @posting = Posting.new(fields)
 
     respond_to do |format|
       if @posting.save
@@ -44,8 +48,10 @@ class PostingsController < ApplicationController
   # PATCH/PUT /postings/1
   # PATCH/PUT /postings/1.json
   def update
+    fields = posting_params
+    fields[:available_dates] = @available_dates if @available_dates
     respond_to do |format|
-      if @posting.update(posting_params)
+      if @posting.update(fields)
         format.html { redirect_to @posting, notice: 'Posting was successfully updated.' }
         format.json { render :show, status: :ok, location: @posting }
       else
@@ -78,6 +84,25 @@ class PostingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def posting_params
-      params.require(:posting).permit(:title, :description, :category_id, :rate, :date_range, :street, :state, :zip, :phone, :email)
+      params.require(:posting).permit(:title, :description, :category_id, :rate, :street, :state, :zip, :phone, :email)
+    end
+
+    def get_date_range
+      if params[:rentrange]
+        from = params[:rentrange][:from]
+        to = params[:rentrange][:to]
+        if from.blank?
+          from = Date.today
+        else
+          from = Date.strptime from, '%m/%d/%Y'
+        end
+        if to.blank?
+          range = from..(Date.today + 1.year)
+        else
+          to = Date.strptime to, '%m/%d/%Y'
+          range = from..to
+        end
+        @available_dates = range
+      end
     end
 end
