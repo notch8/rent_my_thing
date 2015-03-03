1a554ff7a089a9c04d2a1c66c62605b2a65c6a64
LEARN01 <learn01@LEARN01.local>
2015-03-03 13:07:38 -0800

    added administrators to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150302200844) do
-=======
-ActiveRecord::Schema.define(version: 20150302223331) do
->>>>>>> 996a0326f5ca0e37899ae8e5b4f500a870fc74e5
+ActiveRecord::Schema.define(version: 20150303205906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -32,19 +49,14 @@ ActiveRecord::Schema.define(version: 20150302223331) do
     t.integer   "category_id"
     t.decimal   "rate",                       precision: 8, scale: 2
     t.daterange "available_dates"
-<<<<<<< HEAD
     t.string    "street",          limit: 20
-=======
-    t.string    "street",          limit: 2
     t.string    "city"
->>>>>>> 996a0326f5ca0e37899ae8e5b4f500a870fc74e5
     t.string    "zip",             limit: 5
     t.string    "phone"
     t.string    "email"
     t.datetime  "created_at",                                         null: false
     t.datetime  "updated_at",                                         null: false
     t.string    "state",           limit: 2
-    t.string    "city",            limit: 30
   end
 
   add_index "postings", ["category_id"], name: "index_postings_on_category_id", using: :btree
@@ -70,6 +82,7 @@ ActiveRecord::Schema.define(version: 20150302223331) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
