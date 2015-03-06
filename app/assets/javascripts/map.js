addf32437d7795d4b69d53bff7ca272d172ee01a
Jet Martin <jetamartin@gmail.com>
2015-03-06 12:43:34 -0800

    code to add multiple points

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -8,29 +8,40 @@ $(document).on ('page:change', function() {
   }
   // Java script to display map http://openlayers.org/en/v3.2.1/doc/quickstart.html
   var map_div = $('#map')
+  var vectorSource = new ol.source.Vector({
+   //create empty vector
+  });
   if (map_div) {
     // addr = map_div.data('address')
       window.RentMyThing.addresses.forEach(function(addr) {
         get_coords(addr, function(lat, lon) {
           var pt = ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857')
           var rental_loc_icon = new ol.Feature({
-            geometry: new ol.geom.Point(pt)
+            geometry: new ol.geom.Point(pt),
+            name: 'Rental Location'
           })
-          // var renter_loc_icon = new ol.Feature({
-          //   // Need to define renter_item_pts
-          //   // geometry: new ol.geom.Point(rental_item_pts???)
-          // })
-          // renter_loc_icon.setStyle(
-          //   new ol.style.Style({
-          //     image: new ol.style.Icon({
-          //       anchor: [0.5, 46],
-          //       anchorXUnits: 'fraction',
-          //       anchorYUnits: 'pixels',
-          //       opacity: 0.75,
-          //       src: '/images/green-pin.png'
-          //     })
-          //   })
-          // )
+
+          var pt2 = ol.proj.transform([-117.18, 32.92], 'EPSG:4326', 'EPSG:3857')
+          var renter_loc_icon = new ol.Feature({
+              geometry: new ol.geom.Point(pt2),
+              name: 'Renters Location'
+          });
+
+          vectorSource.addFeature(rental_loc_icon);
+          vectorSource.addFeature(renter_loc_icon);
+
+
+          renter_loc_icon.setStyle(
+            new ol.style.Style({
+              image: new ol.style.Icon({
+                anchor: [0.5, 46],
+                anchorXUnits: 'fraction',
+                anchorYUnits: 'pixels',
+                opacity: 0.75,
+                src: '/images/green-pin.png'
+              })
+            })
+          )
 
           rental_loc_icon.setStyle(
             new ol.style.Style({
@@ -43,27 +54,20 @@ $(document).on ('page:change', function() {
               })
             })
           )
-
-          // var markers = new ol.layer.markers( "Markers")
-
+          var vectorLayer = new ol.layer.Vector({
+            source: vectorSource,
+            // style: iconStyle
+          });
           var map = new ol.Map({
             target: 'map',
             layers: [
               new ol.layer.Tile({
                 title: "Rental Proximity Map",
                 source: new ol.source.MapQuest({layer: 'osm'})
-              }),
-              new ol.layer.Vector({
-                source: new ol.source.Vector({
-                  features: [
-                    rental_loc_icon
-                  ]
-                })
-              })
-            ],
+              }), vectorLayer],
             view: new ol.View({
-              center: pt,
-              zoom: 14
+             center: pt,
+              zoom: 12
             }),
             controls: ol.control.defaults({
               attributionOptions: {
