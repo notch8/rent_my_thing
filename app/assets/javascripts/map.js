aba782cda7bdc96bb83fb82d34caa4a5df1a4213
Jet Martin <jetamartin@gmail.com>
2015-03-07 20:04:24 -0800

    changes to allow multiple pins to be plotted and modified javascript to accept coords rather than street addresses

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,89 +1,93 @@
 $(document).on ('page:change', function() {
-  function get_coords(addr, callback) {
-    $.ajax({
-      url: 'http://nominatim.openstreetmap.org/search',
-      data: {q: addr, format: 'json'}})
-      .done(function(data) {
-        if (data.length === 1) {callback(Number(data[0].lat), Number(data[0].lon))}})
-  }
-  // Java script to display map http://openlayers.org/en/v3.2.1/doc/quickstart.html
+
+  // Addresses:
+  // Red pin: Rental item locations
+  // -------------------------------------------
+  // 4625 Texas St., San Diego, CA. 92116     || coords: -117.138475,32.761480
+  // 2267 Boundary St., San Diego, CA. 92104  || coords: -117.116083,32.729552
+  // 4711 Kansas St., San Diego, CA. 92116    || coords: -117.131207,32.763225
+  // 3200 6th Ave., San Diego, CA. 92103      || coords: -117.159727,32.739113
+  // 1642 7th Ave., San Diego, CA. 92104      || coords: -117.159063,32.722439
+
+  // Green pin: Renter's location
+  //-------------------------------------------
+  // 3803 Ray Street, San Diego, CA. 92104    || coords: -117.129137,32.747502
+
+  mapAttributes = {'/images/red-pin.png': [[-117.138475,32.761480], [-117.116083,32.729552],[-117.131207,32.763225],[-117.159727,32.739113], [-117.159063,32.722439]],
+    '/images/green-pin.png': [[-117.129137,32.747502]]}
+
   var map_div = $('#map')
-  var vectorSource = new ol.source.Vector({
-   //create empty vector
-  });
-  if (map_div) {
-    // addr = map_div.data('address')
-      window.RentMyThing.addresses.forEach(function(addr) {
-        get_coords(addr, function(lat, lon) {
-          var pt = ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857')
-          var rental_loc_icon = new ol.Feature({
-            geometry: new ol.geom.Point(pt),
-            name: 'Rental Location'
+  drawMap(mapAttributes);
+
+  function drawMap (mapAttributes) {
+    var iconLocations = [];
+    var vectorSource = new ol.source.Vector({
+     //create empty vector -- not sure if this is needed??????
+    });
+
+    // Object.keys(mapAttributes).forEach(function(pinType) {console.log(pinType);
+    // mapAttributes[pinType].forEach(function(coords){console.log('show pin at ' + coords[0] + ',' + coords[1])})})
+
+    // Outer Loop to retrieve each pin type
+    Object.keys(mapAttributes).forEach(function(pinType) {
+
+
+      // Loop to retrieve all coordinates associated with each pin type
+      mapAttributes[pinType].forEach(function(coords) {
+
+        var iconLocation = ol.proj.transform([coords[0], coords[1]], 'EPSG:4326', 'EPSG:3857')
+        iconLocations.push(iconLocation)
+
+        var iconFeature = new ol.Feature({
+          geometry: new ol.geom.Point(iconLocation),
+        })
+
+        // Create Pin styling
+        iconFeature.setStyle(
+          new ol.style.Style({
+            image: new ol.style.Icon({
+              anchor: [0.5, 46],
+              anchorXUnits: 'fraction',
+              anchorYUnits: 'pixels',
+              opacity: 0.75,
+              src: pinType  // Set pin type
+            })
           })
+        )
+        vectorSource.addFeature(iconFeature);
+      }) // End of inner loop - coords
+    }); // End of outer loop - pinType
 
-          var pt2 = ol.proj.transform([-117.18, 32.92], 'EPSG:4326', 'EPSG:3857')
-          var renter_loc_icon = new ol.Feature({
-              geometry: new ol.geom.Point(pt2),
-              name: 'Renters Location'
-          });
+    // *************Create Vector Layer with Markers and Build Map ***************************
+    var vectorLayer = new ol.layer.Vector({
+      source: vectorSource
+//      style: iconStyle
+    });
 
-          vectorSource.addFeature(rental_loc_icon);
-          vectorSource.addFeature(renter_loc_icon);
+    map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          title: "Rental Proximity Map",
+          source: new ol.source.MapQuest({layer: 'osm'})
+        }), vectorLayer],
+      view: new ol.View({
+       center: iconLocations[0],          // ??? Do i need a centering point at this point.
+        zoom: 12
+      }),
+      controls: ol.control.defaults({
+        attributionOptions: {
+          collapsible: false
+        }}).extend([
+          new ol.control.ScaleLine()
+        ])
+    });
 
-
-          renter_loc_icon.setStyle(
-            new ol.style.Style({
-              image: new ol.style.Icon({
-                anchor: [0.5, 46],
-                anchorXUnits: 'fraction',
-                anchorYUnits: 'pixels',
-                opacity: 0.75,
-                src: '/images/green-pin.png'
-              })
-            })
-          )
-
-          rental_loc_icon.setStyle(
-            new ol.style.Style({
-              image: new ol.style.Icon({
-                anchor: [0.5, 46],
-                anchorXUnits: 'fraction',
-                anchorYUnits: 'pixels',
-                opacity: 0.75,
-                src: '/images/red-pin.png'
-              })
-            })
-          )
-          var vectorLayer = new ol.layer.Vector({
-            source: vectorSource,
-            // style: iconStyle
-          });
-          var map = new ol.Map({
-            target: 'map',
-            layers: [
-              new ol.layer.Tile({
-                title: "Rental Proximity Map",
-                source: new ol.source.MapQuest({layer: 'osm'})
-              }), vectorLayer],
-            view: new ol.View({
-             center: pt,
-              zoom: 12
-            }),
-            controls: ol.control.defaults({
-              attributionOptions: {
-                collapsible: false
-              }}).extend([
-                new ol.control.ScaleLine()
-              ])
-          });
-
-          // Code to make sure map contains all points
-          var view = map.getView()
-          var extent = ol.extent.boundingExtent([pt, pt2])
-          var size = map.getSize()
-          view.fitExtent(extent, size)
-          Window.map = map;
-      })
-    })
-  }
-})
+    // Bound the map
+    var view = map.getView()
+    var extent = ol.extent.boundingExtent(iconLocations)
+    var size = map.getSize()
+    view.fitExtent(extent, size)
+    Window.map = map;
+  } // End of drawmap function
+});
