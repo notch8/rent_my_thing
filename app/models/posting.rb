6c2521849e5e8023a9f4ee51475ff571684f63ef
empireofryan <ryan@ryanclifton.com>
2015-03-12 10:59:45 -0700

    Restore changes from master that were accidentally overriden.

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -3,11 +3,7 @@ class Posting < ActiveRecord::Base
   belongs_to :user
   has_many :reservations
   has_many :reviews
-
-
   ratyrate_rateable "title"
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   has_many :uploads
 
