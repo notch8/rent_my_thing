192f9d41af034d4cde6f24cef6cd1f8fed42e544
Guyren Howe <guyren@relevantlogic.com>
2015-03-24 10:20:36 -0700

    Make the postings view test work by stubbing current_user

--- spec/views/postings/index.html.erb_spec.rb
+++ spec/views/postings/index.html.erb_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe "postings/index.html.erb" do
   it "displays all the postings" do
+
     assign :postings, [
       stub_model(Posting, title: "stub title 1", description: 'stub description2'),
       stub_model(Posting, title: "stub title 2", description: 'stub description1')
     ]
     allow(view).to receive_messages(:will_paginate => nil)
+    allow(view).to receive_messages(:current_user => nil)
 
     render
 
